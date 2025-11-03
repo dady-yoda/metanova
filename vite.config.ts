18604007ffd778f37403914c12ccb505cf427001
dady-yoda <karthikpc037@gmail.com>
2025-11-04 01:17:25 +0530

    fix: update Vercel build configuration and add Node.js version

--- vite.config.ts
+++ vite.config.ts
@@ -14,4 +14,12 @@ export default defineConfig({
       },
     },
   },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'es2020',
+    },
+  },
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' }
+  }
 })
